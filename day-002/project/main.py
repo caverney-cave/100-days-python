8aeedeb49a041c1391c5d84d10ca4f1bab1c5067
caverney-cave <jazulidesign00@gmail.com>
2026-02-13 16:20:32 +0700

    Day 002: Learned data types and f-strings
    - checking data types and how to convert it
    - mathematical operation
    - number manipulation
    - simplifed printing with f-strings
    - create a simple tip calculator

---
+++ day-002/project/main.py
@@ -0,0 +1,11 @@
+print("Welcome to the tip calculator!")
+
+total_bill = float(input("What was the total bill? $"))
+tip = float(input("How much tip would you like to give? 10, 12, or 15? "))
+people_sum = int(input("How many people to split the bill? "))
+
+tip_sum = total_bill * tip / 100
+should_pay = ((total_bill + tip_sum) / people_sum)
+
+# print(f"Each person should pay: {round(should_pay, 2)}")
+print(tip_sum)
